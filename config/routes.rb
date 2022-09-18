eca476772b4e8fabb1f79b8af421d18f04131751
ayya-kh <76466532+ayya-kh@users.noreply.github.com>
2022-09-18 10:08:38 +0000

    [Finish] 3-kadai

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get  '/' => 'homes#top' #プリケーショントップ画面をルートパスに設定
+  root to: 'homes#top' #アプリケーショントップ画面をルートパスに設定
   resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
